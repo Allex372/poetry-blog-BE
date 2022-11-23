06ac704c6a2b7d1fd3cea4e8331fe4df6e93805d
oleksandr <alexalex372.aa@gmail.com>
2022-11-23 15:30:13 +0200

    change logic

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     MONGO_URL: 'mongodb://localhost:27017/Blogger',
     // MONGO_URL: 'mongodb+srv://Olleksandr:zIyLM88buYClq1M6@cluster0.ysxtq8s.mongodb.net/?retryWrites=true&w=majority',
-    PORT: 5000,
+    PORT: process.env.PORT || 5000,
     JWT_ACCESS: process.env.JWT_ACCESS || 'JWT_ACCESS',
     JWT_REFRESH: process.env.JWT_REFRESH || 'JWT_REFRESH',
 
